28778b1e72a67b8e8d1fc93004897b4311c3125c
pranjalmittal01 <pranjalmittal810@gmail.com>
2024-03-12 12:49:20 +0530

    adding deletenote functionality on the server side by using api call

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -49,8 +49,18 @@ const NoteState = (props) => {
     }
 
     // Functionality of Delete Note
-    const deleteNote = (_id) => {
-        // TODO API Call
+    const deleteNote = async (_id) => {
+        // API Call
+        const response = await fetch(`${host}/api/notes/deletenote/${_id}`, {
+            method: "DELETE", 
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjVlNTZjYjFhY2VlM2QwMGM3NGY3MTg1In0sImlhdCI6MTcwOTcwNjA4NX0.zxurVV5FRK27Dbel-ZvQXodbhAGwsMmKJ6pZKCJu790"
+            }
+        });
+        const json =  response.json(); 
+        console.log(json);
+
         console.log("Deleting the Note with id " + _id);
         const newNotes = notes.filter((note) => { return note._id !== _id })
         setNotes(newNotes);
